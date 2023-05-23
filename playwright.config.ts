041b61fecea1e6d0b7f1c9325429919a6fbc4589
Alin Florin <alin@qoffice.ro>
2023-05-23 17:23:53 +0300

    -

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,20 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import { defineConfig, devices } from "@playwright/test";
 
 dotenv.config();
+
+export default defineConfig({
+  projects: [
+    { name: "setup", testMatch: /.*\.setup\.ts/ },
+
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: ".auth/user.json",
+      },
+      dependencies: ["setup"],
+    },
+  ],
+
+});
---
+++ tests/auth.setup.ts
@@ -0,0 +1,20 @@
+import { test as setup } from "@playwright/test";
+import endpoint from "./configTypes";
+
+const authFile = ".auth/user.json";
+
+setup("authenticate", async ({ page }) => {
+  // Go to the Droplets product page of DigitalOcean web page
+  await page.goto(endpoint.HUNA_URL + '/login');
+  // Wait for the page to load
+  await page.waitForLoadState("networkidle");
+  await page.getByRole("button", { name: "Login with Microsoft" }).click();
+  await page.waitForURL(x => x.toString().startsWith("https://login.microsoftonline.com/"), {waitUntil: 'networkidle'});
+  await page.getByLabel("Email, phone, or Skype").fill(process.env.HUNA_LOGIN_EMAIL!);
+  await page.getByLabel("Email, phone, or Skype").press("Enter", {delay: 100});
+  await page.getByLabel("Password").fill(process.env.HUNA_LOGIN_PASSWORD!);
+  await page.getByLabel("Password").press("Enter", {delay: 100});
+  await page.getByRole("button", { name: "Yes" }).click({delay: 1000});
+  await page.waitForURL(endpoint.HUNA_URL);
+  await page.context().storageState({ path: authFile });
+});
